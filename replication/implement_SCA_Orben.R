e88bc92c8d64abaa7e6926c40983610b6f9d5870
wxindu <wendu@reed.edu>
2020-04-24 05:02:30 -0700

    updated sca plot

--- replication/implement_SCA_Orben.R
+++ replication/implement_SCA_Orben.R
@@ -105,6 +105,37 @@ linear_coef <- lapply(linear_summary, function(x) x$coefficients[2,1])
 linear_pvalues <- lapply(linear_summary, function(x) x$coefficients[2,4])
 mean(linear_coef > 0)
 mean(linear_pvalues < 0.05)
+mean(linear_coef < 0 & linear_pvalues < 0.05)
+
+model_spec <- data.frame(unlist(formulas)) %>%
+  mutate(plan_suicide = grepl("q27_n", unlist.formulas., fixed = TRUE), 
+         serious_consid_suicide = grepl("q26_n", unlist.formulas., fixed = TRUE), 
+         felt_hopeless = grepl("q25_n", unlist.formulas., fixed = TRUE), 
+         attempt_suicide = grepl("q28_nd", unlist.formulas., fixed = TRUE), 
+         recent_alcohol = grepl("q42", unlist.formulas., fixed = TRUE), 
+         halluc_drug  = grepl("qhallucdrug ", unlist.formulas., fixed = TRUE), 
+         int_hours_computer = grepl("* q81_n", unlist.formulas., fixed = TRUE), 
+         int_physical_activity = grepl("* q79", unlist.formulas., fixed = TRUE),
+         int_recent_alcohol = grepl("* q42", unlist.formulas., fixed = TRUE),
+         int_halluc_drug = grepl("* qhallucdrug", unlist.formulas., fixed = TRUE))%>%
+  rename(formula = unlist.formulas.)
+results %>% full_join(model_spec) %>% filter(sig < 0.05) %>% 
+  summarize(mean_plan_suicide = mean(plan_suicide), 
+            mean_serious_consid_suicide = mean(serious_consid_suicide), 
+            mean_hopeless = mean(felt_hopeless), 
+            mean_attempt_suicide = mean(attempt_suicide), 
+            mean_recent_alcohol = mean(recent_alcohol), 
+            mean_halluc_drug = mean(halluc_drug), 
+            mean_int_computer = mean(int_hours_computer), 
+            mean_int_physical_active = mean(int_physical_activity), 
+            mean_int_recent_alcohol = mean(int_recent_alcohol), 
+            mean_int_halluc_drug = mean(int_halluc_drug))
+results %>% full_join(model_spec) %>% filter(int_halluc_drug == TRUE) %>% 
+  summarize(pos = mean(coef > 0), sig = mean(p < 0.05), pos_sig = mean(coef >0 & p < 0.05))
+results %>% full_join(model_spec) %>% filter(int_physical_activity == TRUE)
+mean(results$coef > 0)
+mean(results$p < 0.05)
+mean(results$coef > 0 & results$p < 0.05)
 
 length(unlist(logit_coef))
 unlist(logit_pvalues)
@@ -117,6 +148,7 @@ results <- data.frame(coef = c(unlist(logit_coef), unlist(linear_coef)),
            p = c(unlist(logit_pvalues), unlist(linear_pvalues)), 
            model = c(rep("logit", 64), rep("linear", 64)), 
            formula = unlist(formulas)) %>%
+             mutate(formula = paste(formula, model)) %>%
   arrange(coef) %>%
   mutate(model_number = 1:128) %>%
   mutate(sig = ifelse(p < 0.05, TRUE, FALSE))
@@ -130,41 +162,50 @@ curve <- results %>% ggplot(aes(x = model_number, y = coef, color = sig)) + geom
                                    size = 0.5, linetype = "solid")) + 
   geom_hline(yintercept=0, linetype="dashed", color = "grey") +
   theme(legend.position = "left") +
-  labs(color = "P-value < 0.05                ", 
+  labs(color = "P-value < 0.05             ", 
        x = "Model Number", 
        y = "Estimated Coefficients") +
   scale_x_continuous(breaks = seq(0, 128, by = 10)) 
 
-spec_plot <- data.frame(unlist(formulas)) %>%
-  mutate(plan_suicide = grepl("q27_n", unlist.formulas., fixed = TRUE), 
-         serious_consid_suicide = grepl("q26_n", unlist.formulas., fixed = TRUE), 
-         felt_hopeless = grepl("q25_n", unlist.formulas., fixed = TRUE), 
-         attempt_suicide = grepl("q28_nd", unlist.formulas., fixed = TRUE), 
-         recent_alcohol = grepl("q42", unlist.formulas., fixed = TRUE), 
-         halluc_drug  = grepl("qhallucdrug ", unlist.formulas., fixed = TRUE), 
-         int_hours_computer = grepl("* q81_n", unlist.formulas., fixed = TRUE), 
-         int_physical_activity = grepl("* q79", unlist.formulas., fixed = TRUE),
-         int_recent_alcohol = grepl("* q42", unlist.formulas., fixed = TRUE),
-         int_halluc_drug = grepl("* qhallucdrug", unlist.formulas., fixed = TRUE)) %>%
-  gather(key = "spec", value = "T_F", plan_suicide:int_halluc_drug) %>% 
+spec_plot <- results %>%
+  mutate(plan_suicide = grepl("q27_n", formula, fixed = TRUE), 
+         serious_consid_suicide = grepl("q26_n", formula, fixed = TRUE), 
+         felt_hopeless = grepl("q25_n", formula, fixed = TRUE), 
+         attempt_suicide = grepl("q28_nd", formula, fixed = TRUE), 
+         recent_alcohol = grepl("q42", formula, fixed = TRUE), 
+         halluc_drug  = grepl("qhallucdrug ", formula, fixed = TRUE), 
+         int_hours_computer = grepl("* q81_n", formula, fixed = TRUE), 
+         int_physical_activity = grepl("* q79", formula, fixed = TRUE),
+         int_recent_alcohol = grepl("* q42", formula, fixed = TRUE),
+         int_halluc_drug = grepl("* qhallucdrug", formula, fixed = TRUE), 
+         logit = grepl("logit", formula, fixed = TRUE), 
+         linear = grepl("linear", formula, fixed = TRUE)) %>%
+  mutate(No_add_control = (recent_alcohol == FALSE & halluc_drug == FALSE), 
+         No_int = (int_hours_computer == FALSE & 
+                     int_physical_activity == FALSE &
+                     int_recent_alcohol == FALSE & 
+                     int_halluc_drug == FALSE)) %>%
+  gather(key = "spec", value = "T_F", plan_suicide:No_int) %>% 
   filter(T_F == TRUE) %>%
-  rename(formula = unlist.formulas.) %>%
   full_join(spec_name) %>%
   mutate(specification = factor(specification, levels = c("TV use * hours of computer", 
                                                           "TV use * days of physical active", 
                                                           "TV use * recent alcohol consumption", 
                                                           "TV use * hallucinogenic drug consumption", 
+                                                          "No interaction term",
                                         "INTERACTION TERMS", 
                                         "Recent alcohol consumption", 
                                         "Recent hallucinogenic drug consumption", 
+                                        "No additional control variables",
                                         "ADDITIONAL CONTROL VARIABLES", 
                                         "Planned on suicide", 
                                         "Seriously considered suicide", 
                                         "Felt hopeless and stopped usual activities", 
                                         "Actual attempt on suicide", 
-                                        "ALTERNATIVE RESPONSE VARIABLES"))) %>%
-  full_join(results) %>%
-  ggplot(aes(x = model_number, y = specification)) + geom_point() +
+                                        "ALTERNATIVE RESPONSE VARIABLES", 
+                                        "Linear Regression", "Logit Regression", "MODEL FORM"))) %>%
+  mutate(sig = p < 0.05) %>%
+  ggplot(aes(x = model_number, y = specification, col = sig)) + geom_point() +
   theme_classic() +
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
@@ -172,7 +213,8 @@ spec_plot <- data.frame(unlist(formulas)) %>%
         axis.line.x=element_blank(), 
         axis.line.y = element_line(colour = "grey", 
                                  size = 0.5, linetype = "solid"), 
-        axis.title.y = element_blank())
+        axis.title.y = element_blank(), 
+        legend.position='none')
 
 spec_name <- data.frame(spec = c("int_hours_computer", "int_physical_activity", 
                     "int_recent_alcohol", "int_halluc_drug", 
@@ -180,7 +222,12 @@ spec_name <- data.frame(spec = c("int_hours_computer", "int_physical_activity",
                     "plan_suicide", "serious_consid_suicide", "felt_hopeless", "attempt_suicide", 
                     "ADDITIONAL CONTROL VARIABLES", 
                     "ALTERNATIVE RESPONSE VARIABLES", 
-                    "INTERACTION TERMS"), 
+                    "INTERACTION TERMS", 
+                    "logit", 
+                    "linear",
+                    "MODEL FORM", 
+                    "No_add_control", 
+                    "No_int"), 
            specification = c("TV use * hours of computer", 
                              "TV use * days of physical active", 
                              "TV use * recent alcohol consumption", 
@@ -193,6 +240,9 @@ spec_name <- data.frame(spec = c("int_hours_computer", "int_physical_activity",
                              "Actual attempt on suicide", 
                              "ADDITIONAL CONTROL VARIABLES", 
                              "ALTERNATIVE RESPONSE VARIABLES", 
-                             "INTERACTION TERMS"))
+                             "INTERACTION TERMS", 
+                             "Logit Regression", "Linear Regression", "MODEL FORM", 
+                             "No additional control variables", 
+                             "No interaction term"))
 SCA_yrbs <- grid.arrange(curve, spec_plot, ncol = 1)
 ggsave(here("replication", "SCA_yrbs_TV_suicide.jpg"), SCA_yrbs, width = 12, height = 8)
